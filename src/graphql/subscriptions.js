5c5cb116b33aab31c51575b8d461cfe5cf14e379
Shumin Guo <gsmsteve@gmail.com>
2020-01-13 17:21:57 -0800

    Added the schema generated code for graphql.

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    note
+    done
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    note
+    done
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    note
+    done
+  }
+}
+`;
